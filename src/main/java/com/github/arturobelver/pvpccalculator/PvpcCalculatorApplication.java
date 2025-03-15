4ed79c9641fd26901ec7ee08068d4a5f8e46bd77
Arturo Belmonte Vergel <arturobelver@gmail.com>
2025-03-15 13:37:28 +0100

    config: initial commit

---
+++ src/main/java/com/github/arturobelver/pvpccalculator/PvpcCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.arturobelver.pvpccalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvpcCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PvpcCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/arturobelver/pvpccalculator/PvpcCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arturobelver.pvpccalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvpcCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
